6096a8ca2ec19f6515f469ee138bd36bcfc1287a
ByoungHwa Lee <onlyforheart@hanmail.net>
2024-12-05 17:01:06 +0900

    서버 역할별 데이터 처리 흐름 변경

--- server/data_processor.py
+++ server/data_processor.py
@@ -1,166 +1,174 @@
-from collections import defaultdict
-import json
 from datetime import datetime
-import requests
+import time
+import psutil
+import json
+
 from config import Config
+from utils.logger import server_logger
+from schemas.market_data import MarketData
 
 class DataProcessor:
-    def __init__(self, db_manager, kafka_handler, consumer):
+    def __init__(self, db_manager):
         self.db_manager = db_manager
-        self.kafka_handler = kafka_handler
-        self.consumer = consumer
-        self.metadata = []
         self.dataset = []
-        self.dsHistorical = []
-        self.dsToday = []
-        self.index = defaultdict(list)
-
-    # def process_real_time_data(self, market_data):
-    #     """실시간 데이터 처리"""
-    #     try:
-    #         processed_data = {
-    #             'source': str(market_data.source),
-    #             'timestamp': str(market_data.timestamp),
-    #             'data_type': str(market_data.dataType),
-    #             'content': [{
-    #                 'item_code': str(item.itemCode),
-    #                 'current_price': float(item.currentPrice)
-    #             } for item in market_data.content]
-    #         }
-    #         self.dataset.append(processed_data)
-    #         print(f"실시간 데이터가 처리되었습니다. 데이터 수: {len(self.dataset)}")
-    #     except Exception as e:
-    #         print(f"실시간 데이터 처리 오류: {e}")
-
-    # def process_batch(self):
-    #     """배치 데이터 처리"""
-    #     # 배치 처리 로직 구현
-    #     pass
-
-    # def complete_dataset(self):
-    #     """전체 데이터셋 완성"""
-    #     self.load_metadata()  # 메타데이터 로드
-    #     self.load_today_data()  # 오늘자 데이터 로드
+        self.last_db_write = time.time()
+        self.running = True
         
-    #     # 메시지 큐로부터 실시간 데이터 수신 및 처리
-    #     while self.running:
-    #         messages = self.consumer.consume(num_messages=100, timeout=1.0)
-    #         for msg in messages:
-    #             if not msg.error():
-    #                 self.process_real_time_data(json.loads(msg.value().decode('utf-8')))
+    def check_idle_time(self):
+        """
+        CPU 사용률 30% 이하, 메모리 여유 30% 이상일 때
+        프로그램 시작 시점부터 10분 단위로 메모리 데이터를 임시 테이블에 기록
+        """
+        
+        # 프로그램 시작 시간 기록
+        start_time = time.time()
+        interval = Config.INTERVAL  # 10분 = 600초
+        
+        while self.running:  # running 플래그 확인
+            try:
+                # CPU, 메모리 사용률 체크
+                cpu_usage = psutil.cpu_percent(interval=1)
+                memory = psutil.virtual_memory()
+                memory_available = memory.available * 100 / memory.total
+                
+                # idle 상태 체크
+                if cpu_usage <= Config.CPU_USAGE and memory_available >= Config.MEMORY_ABAILABLE:
+                    # 시작 시간부터 interval 단위 경과 체크
+                    elapsed_time = time.time() - start_time
+                    if elapsed_time >= interval:
+                        # DB 저장 로직 호출
+                        self.send_data_to_db(time.time())
+                        print(f"[{datetime.now()}] {interval}초 기간 실시간 데이터 보관: 현재까지 총 {len(self.dataset)}개")
+                        # 다음 interval 주기를 위해 시작 시간 갱신
+                        start_time = time.time()
+                
+                # 데이터셋 만료 체크 및 저장
+                self.check_and_store_data()
 
-    def load_initial_data(self):
-        """DB에서 과거 데이터 및 당일 실시간 데이터 로드"""
-        self.metadata = self.db_manager.load_marketdata_meta()  # 메타 데이터 로드
+                # 1분 대기
+                time.sleep(60)
+                
+            except Exception as e:
+                print(f"데이터 이관 중 오류 발생: {e}")
+                if not self.running:  # 종료 중이면 루프 탈출
+                    break
+                time.sleep(60)
 
-        # self.load_historical_data()  # 과거 데이터 로드
-        self.load_today_data() # 당일 데이터 로드
-
-        # TODO: 데이터셋 결합, 중복 제거 등 처리
-        # self.dataset = _combine_data(self.dsHistorical, today_data)
-        # self.dataset = self.remove_duplicates(self.dataset)  # 중복 제거
-
-
-    def load_historical_data(self):
-        dictDS_price = {
-            'data_type': 'price', 
-            'time_series': '', 
-            'content': [{
-                'item_code': '', 
-                'timestamp': '', 
-                'open': None, 
-                'high': None, 
-                'low': None, 
-                'close': None,
-                'volume': None
-            }]
-        }
-
-        dictDS_price['time_series'] = 'daily'
-        dictDS_price['content'] = self.db_manager.load_marketdata_price_daily()
-        self.dsHistorical.extend(dictDS_price)
-        # dictDS_price['time_series'] = '1m'
-        # dictDS_price['content'] = self.db_manager.load_marketdata_price_1m()
-        # self.dsHistorical.append(dictDS_price)
-        # dictDS_price['time_series'] = 'tick'
-        # dictDS_price['content'] = self.db_manager.load_marketdata_price_tick()
-        # self.dsHistorical.append(dictDS_price)
-
-        print("마켓 일별 데이터 로드 완료: {}건".format(len(self.dsHistorical)))
-        self.dataset = self.dsHistorical
-
-    def load_today_data(self):
-        today = datetime.today().date()
-        stored_data = self.db_manager.load_marketdata_price_rt(today)    # 오늘자 실시간 데이터 적재분 로드
-        if stored_data:
-            # TODO: 오늘자 데이터(틱)로 분봉 등 생성 처리
-            # TODO: dataset 형식에 맞게 정리
-            # self.dataset.extend(stored_data)
-            print(f"오늘자 실시간 데이터 로드 완료: {len(stored_data)}건")
-        else:
-            print("현재 시점에서 로드할 오늘자 데이터 적재분은 없습니다")
-        # missing_data = self.request_missing_data()  # 오늘자 실시간 데이터 미적재분 요청
-        # if missing_data:
-        #     # self.dataset.extend(missing_data)
-        #     print(f"메인 서버로부터 실시간 데이터 데이터 수신 완료: {len(missing_data)}건")
-        # else:
-        #     print("메인 서버로부터 수신한 실시간 데이터는 없습니다")
-
-    def request_missing_data(self):
-        """메인 서버에 누락된 데이터 요청"""
-        try:
-            response = requests.get("http://localhost:5000/api/missing_data")
-            if response.status_code == 200:
-                missing_data = response.json()
-                return missing_data
-            else:
-                print(f"데이터 요청 실패: {response.status_code} - {response.text}")
-                return []
-        except Exception as e:
-            print(f"누락된 데이터 요청 중 오류 발생: {e}")
-            return []
+    def send_data_to_db(self, current_time):
+        """데이터를 DB에 저장하는 메서드"""
+        new_data = [data for data in self.dataset if data['received_at'] > self.last_db_write]
+        if new_data:
+            prepared_data = self.prepare_db_data(new_data)
+            self.db_manager.insert_to_marketdata_price_rt(prepared_data)
+            self.last_db_write = current_time
+            print(f"새로운 데이터 {len(new_data)}개가 DB에 저장되었습니다.")
     
-    def remove_duplicates(self, data):
-        """중복된 데이터 제거"""
-        seen = set()
-        unique_data = []
-        
-        for item in data:
-            identifier = (item['content'][0]['item_code'], item['timestamp'])  # 고유 식별자
-            if identifier not in seen:
-                seen.add(identifier)
-                unique_data.append(item)
-        
-        return unique_data
+    def check_and_store_data(self):
+        """데이터셋을 DB로 전송하고 비우는 메서드"""
+        if not self.dataset:
+            return
 
-    def process_kafka_data(self, market_data):
-        """Kafka로부터 수신된 실시간 데이터 처리"""
+        # 데이터셋의 첫 번째 데이터의 날짜를 기준으로 다음날인지 확인
+        first_data_date = datetime.fromisoformat(self.dataset[0]['timestamp']).date()
+        current_date = datetime.now().date()
+        current_time = time.time()
+
+        if current_date > first_data_date:
+            # 전송할 데이터와 남길 데이터 분리
+            data_to_store = [data for data in self.dataset if datetime.fromisoformat(data['timestamp']).date() < current_date]
+            data_to_keep = [data for data in self.dataset if datetime.fromisoformat(data['timestamp']).date() == current_date]
+
+            # DB에 저장
+            for data in data_to_store:
+                prepared_data = self.prepare_db_data(data)
+                self.db_manager.insert_to_marketdata_price_rt(prepared_data)
+            print(f"데이터셋이 DB에 저장되었습니다. 저장된 데이터 수: {len(data_to_store)}")
+
+            # 데이터셋 비우고 남길 데이터만 유지
+            self.dataset = data_to_keep
+            self.last_db_write = current_time
+
+    def is_capnp_data(self, data):
+        """데이터가 Cap'n Proto 형식인지 확인하는 메서드"""
+        # Cap'n Proto 데이터는 일반적으로 바이너리 형식이므로, 특정 바이트 패턴을 확인할 수 있습니다.
+        # 예를 들어, 첫 번째 바이트가 특정 값인지 확인하는 방법
+        return isinstance(data, bytes) and data.startswith(b'\x00')  # 예시로 첫 바이트가 0x00인지 확인
+
+    def process_data(self, data, client_id):
+        if self.is_capnp_data(data):
+            processed_data = self.process_capnp_data(data, client_id)
+        else:
+            processed_data = self.process_json_data(data, client_id)
+
+        return processed_data
+
+    def process_capnp_data(self, data, client_id):
+        """Cap'n Proto 데이터 처리"""
+        with MarketData.MarketData.from_bytes(data) as market_data:
+            if not self.socket_handler.validate_market_data(market_data):
+                server_logger.warning(f"Invalid Cap'n Proto data from {client_id}")
+                return
+
+            processed_data = self._process_data(market_data)
+
+            return processed_data
+
+    def process_json_data(self, data, client_id):
+        """JSON 데이터 처리"""
         try:
-            received_data = {
-                'source': market_data['source'],
-                'timestamp': market_data['timestamp'],
-                'data_type': market_data['data_type'],
+            current_time = time.time()
+            if isinstance(data, bytes):
+                data = data.decode('utf-8')
+            json_data = json.loads(data)  # JSON 문자열을 파싱
+            # JSON 데이터 처리 로직 추가
+            processed_data = {
+                'source': json_data['source'],
+                'timestamp': json_data['timestamp'],
+                'data_type': json_data['data_type'],
                 'content': [{
                     'item_code': item['item_code'],
-                    'current_price': item['current_price']
-                } for item in market_data['content']]
+                    'current_price': float(item['current_price']),
+                    'current_vol': float(item['current_vol'])
+                } for item in json_data['content']],
+                'received_at': current_time}
+
+            return processed_data
+        except json.JSONDecodeError as e:
+            server_logger.warning(f"Invalid JSON data from {client_id}: {e}")
+
+    def _process_data(self, market_data):
+        try:
+            current_time = time.time()
+            processed_data = {
+                'source': str(market_data.source),
+                'timestamp': str(market_data.timestamp),
+                'data_type': str(market_data.dataType),
+                'content': [{
+                    'item_code': str(item.itemCode),
+                    'current_price': float(item.currentPrice)
+                } for item in market_data.content],
+                'received_at': current_time
             }
 
-            # 데이터셋에 추가하고 중복 제거
-            self.dataset.append(received_data)
-
-            # Kafka로 가공된 데이터 전송
-            self.kafka_handler.send_data(received_data, topic=Config.KAFKA_TOPICS['PROCESSED_DATA'])
-
+            return processed_data
+            
         except Exception as e:
-            print(f"[Processing Server] 실시간 데이터 수신 오류: {e}")
-
-    def manage_data_flow(self):
-        """전체 데이터 흐름 관리"""
-        messages = self.consumer.consume(num_messages=100, timeout=1.0)
-        for msg in messages:
-            if not msg.error():
-                market_data = json.loads(msg.value().decode('utf-8'))
-                self.process_kafka_data(market_data)
-        
-        # self.save_to_db()  # 통합된 데이터셋을 DB에 저장
+            server_logger.error("데이터 처리 오류: %s", e, exc_info=True)
+            return None
+    
+    def prepare_db_data(self, new_data):
+        """DB 저장을 위한 데이터 형식 변환"""
+        db_records = []
+        for data in new_data:
+            # ISO 형식의 timestamp를 파싱 (예: 2024-11-13T17:13:34.622521)
+            dt = datetime.fromisoformat(data['timestamp'])
+            formatted_timestamp = dt.strftime('%Y-%m-%d %H:%M:%S.%f')
+            
+            for item in data['content']:
+                db_records.append({
+                    'symbol': item['item_code'],
+                    'timestamp': formatted_timestamp,
+                    'price': item['current_price'],
+                    'volume': item['current_vol']
+                })
+        return db_records
--- server/main_server.py
+++ server/main_server.py
@@ -3,62 +3,46 @@ import socket
 import threading
 import os
 import sys
-import psutil
 import signal
-from datetime import datetime
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import Config
 from database.db_manager import DatabaseManager
 from server.handlers.socket_handler import SocketHandler
-from server.market_data_processor import MarketDataProcessor
 from server.handlers.kafka_handler import KafkaHandler
-from confluent_kafka import Producer
 from server.monitoring.system_monitor import SystemMonitor
-from server.processing_server import ProcessingServer
+from server.data_processor import DataProcessor
 from utils.logger import server_logger
 
 
-# app = Flask(__name__)
-# class DataServer(Flask):
 class DataServer():
     def __init__(self, host=Config.HOST, port=Config.PORT, monitor=None):
-        # super().__init__(import_name=__name__, static_url_path='')  # static_url_path를 빈 문자열로 설정
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.settimeout(1.0)  # accept()에 대한 타임아웃만 설정
         self.host = host
         self.port = port
         self.clients = []
         self.client_data = {}  # 각 클라이언트별 수신된 데이터 저장
-        self.dataset = []      # 통합 데이터셋
         self.active_clients = {}  # 활성 클라이언트 관리
         self.client_locks = {}    # 클라이언트별 락
         self.connection_status = {}  # 연결 상태 모니터링
         self.max_clients = Config.MAX_CLIENTS  # 최대 클라이언트 수 제한
         self.memory_limit = Config.MEMORY_LIMIT  # 메모리 사용량 제한
-        self.last_db_write = time.time()  # 마지막 DB 기록 시점
         self.db_manager = DatabaseManager()
         self.socket_handler = SocketHandler()
         self.kafka_handler = KafkaHandler()
-        self.marketdata_processor = MarketDataProcessor(
-            self.client_data, 
-            self.dataset,
-            self.socket_handler,
-            self.kafka_handler
-        )
         self.running = True
         self.monitor = monitor
-        # self.run(host=self.host, port=self.port, debug=True)
+        self.data_processor = DataProcessor(self.db_manager)
+        
+        # 데이터 처리 스레드 시작
+        self.processor_thread = threading.Thread(target=self.data_processor.check_idle_time, daemon=True)
+        self.processor_thread.start()
         
     def start(self):
         try:
             self.server_socket.bind((self.host, self.port))
             self.server_socket.listen()
-            
-            # 주기적인 DB 업데이트를 위한 스레드 시작
-            self.idle_thread = threading.Thread(target=self.check_idle_time, daemon=True)
-            self.idle_thread.start()
-            
             server_logger.info("서버가 시작되었습니다.")
             
             # 메인 서버 루프 실행
@@ -96,80 +80,6 @@ class DataServer():
                 server_logger.error(f"서버 루프 오류: {e}", exc_info=True)
                 break
 
-    def send_data_to_db(self, current_time):
-        """데이터를 DB에 저장하는 메서드"""
-        new_data = [data for data in self.dataset if data['received_at'] > self.last_db_write]
-        if new_data:
-            prepared_data = self.marketdata_processor.prepare_db_data(new_data)
-            self.db_manager.insert_to_marketdata_price_rt(prepared_data)
-            self.last_db_write = current_time
-            print(f"새로운 데이터 {len(new_data)}개가 DB에 저장되었습니다.")
-    
-    def check_and_store_data(self):
-        """데이터셋을 DB로 전송하고 비우는 메서드"""
-        if not self.dataset:
-            return
-
-        # 데이터셋의 첫 번째 데이터의 날짜를 기준으로 다음날인지 확인
-        first_data_date = datetime.fromisoformat(self.dataset[0]['timestamp']).date()
-        current_date = datetime.now().date()
-        current_time = time.time()
-
-        if current_date > first_data_date:
-            # 전송할 데이터와 남길 데이터 분리
-            data_to_store = [data for data in self.dataset if datetime.fromisoformat(data['timestamp']).date() < current_date]
-            data_to_keep = [data for data in self.dataset if datetime.fromisoformat(data['timestamp']).date() == current_date]
-
-            # DB에 저장
-            for data in data_to_store:
-                prepared_data = self.marketdata_processor.prepare_db_data(data)
-                self.db_manager.insert_to_marketdata_price_rt(prepared_data)
-            print(f"데이터셋이 DB에 저장되었습니다. 저장된 데이터 수: {len(data_to_store)}")
-
-            # 데이터셋 비우고 남길 데이터만 유지
-            self.dataset = data_to_keep
-            self.last_db_write = current_time
-
-    def check_idle_time(self):
-        """
-        CPU 사용률 30% 이하, 메모리 여유 30% 이상일 때
-        프로그램 시작 시점부터 10분 단위로 메모리 데이터를 임시 테이블에 기록
-        """
-        
-        # 프로그램 시작 시간 기록
-        start_time = time.time()
-        interval = Config.INTERVAL  # 10분 = 600초
-        
-        while self.running:  # running 플래그 확인
-            try:
-                # CPU, 메모리 사용률 체크
-                cpu_usage = psutil.cpu_percent(interval=1)
-                memory = psutil.virtual_memory()
-                memory_available = memory.available * 100 / memory.total
-                
-                # idle 상태 체크
-                if cpu_usage <= Config.CPU_USAGE and memory_available >= Config.MEMORY_ABAILABLE:
-                    # 시작 시간부터 interval 단위 경과 체크
-                    elapsed_time = time.time() - start_time
-                    if elapsed_time >= interval:
-                        # DB 저장 로직 호출
-                        self.send_data_to_db(time.time())
-                        print(f"[{datetime.now()}] {interval}초 기간 실시간 데이터 보관: {len(self.dataset)} 완료")
-                        # 다음 interval 주기를 위해 시작 시간 갱신
-                        start_time = time.time()
-                
-                # 데이터셋 만료 체크 및 저장
-                self.check_and_store_data()
-
-                # 1분 대기
-                time.sleep(60)
-                
-            except Exception as e:
-                print(f"데이터 이관 중 오류 발생: {e}")
-                if not self.running:  # 종료 중이면 루프 탈출
-                    break
-                time.sleep(60)
-        
     def handle_client(self, client_socket, client_id):
         """클라이언트 연결 처리"""
         self.socket_handler.setup_client(client_socket, client_id)
@@ -190,10 +100,10 @@ class DataServer():
                 self.socket_handler.reconnect_attempts[client_id] = 0
 
                 # 데이터 처리
-                processed_data = self.marketdata_processor.process_data(data, client_id)
+                processed_data = self.data_processor.process_data(data, client_id)
                 if processed_data:
+                    self.data_processor.dataset.append(processed_data)  # 데이터셋에 추가
                     self.kafka_handler.send_data(processed_data, topic=Config.KAFKA_TOPICS['RAW_MARKET_DATA']) # 실시간으로 Kafka에 전송
-                    self.dataset.append(processed_data)    # 데이터셋에 추가
 
             except Exception as e:
                 server_logger.error(f"Client {client_id} error: {e}")
@@ -212,32 +122,14 @@ class DataServer():
         if client_id in self.socket_handler.connection_status:
             del self.socket_handler.connection_status[client_id]
         
-    # def monitor_memory_usage(self):
-    #     """메모리 사용량 모니터링"""
-    #     process = psutil.Process()
-    #     memory_usage = process.memory_info().rss / 1024 / 1024  # MB
-        
-    #     if memory_usage > self.memory_limit:
-    #         # 오래된 데이터 정리
-    #         self.cleanup_old_data()
-
-    # def cleanup_old_data(self):
-    #     """오래된 데이터 정리"""
-    #     # 오래된 데이터 기준 시간 설정 (예: 1시간 전)
-    #     cutoff_time = time.time() - 3600  # 1시간 전
-
-    #     # 오래된 데이터 제거
-    #     self.dataset = [data for data in self.dataset if data['received_at'] > cutoff_time]
-    #     print(f"오래된 데이터가 정리되었습니다. 남은 데이터 수: {len(self.dataset)}")
-
     def stop(self):
         """서버 종료 메서드"""
         self.running = False
         self.server_socket.close()
         
         # 실행 중인 모든 스레드 정리
-        if hasattr(self, 'idle_thread') and self.idle_thread.is_alive():
-            self.idle_thread.join(timeout=5)
+        if hasattr(self, 'processor_thread') and self.processor_thread.is_alive():
+            self.processor_thread.join(timeout=5)
         
         # Kafka 핸들러 종료
         if hasattr(self, 'kafka_handler'):
@@ -250,11 +142,6 @@ def start_data_server(system_monitor):
     """데이터 서버 시작"""
     data_server = DataServer(monitor=system_monitor)
     data_server.start()
-
-def start_processing_server():
-    """처리 서버 시작"""
-    processing_server = ProcessingServer()
-    processing_server.start()
 
 if __name__ == "__main__":
     """서버 시작"""
@@ -274,26 +161,22 @@ if __name__ == "__main__":
                 data_server.stop()
             if processing_server:
                 processing_server.stop()
-            return False
-        
+            sys.exit(1)
+            
         signal.signal(signal.SIGINT, signal_handler)
         signal.signal(signal.SIGTERM, signal_handler)
         
         # 서버를 스레드로 시작
         server_logger.info("데이터 서버와 처리 서버를 시작합니다...")
         data_server_thread = threading.Thread(target=start_data_server, args=(system_monitor,))
-        processing_server_thread = threading.Thread(target=start_processing_server)
 
         data_server_thread.start()
-        processing_server_thread.start()
 
         # 메인 스레드에서 스레드가 종료될 때까지 대기
         data_server_thread.join()
-        processing_server_thread.join()
         
     except KeyboardInterrupt:
         signal_handler(signal.SIGINT, None)
 
     except Exception as e:
         server_logger.error(f"서버 시작 중 오류 발생: {str(e)}", exc_info=True)
-        sys.exit(1)
